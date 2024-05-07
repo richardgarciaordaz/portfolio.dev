fb25dc741edbe06dc5be3aa4fbf80c02557fc65f
richardgarciaordaz <rchrdgrcordaz@gmail.com>
2024-05-07 01:38:31 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import astroI18next from "astro-i18next";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [astroI18next(),tailwind()],
 });
