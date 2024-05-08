c6b122b8d20ce3faafc26f7105e6110644b87fdd
richardgarciaordaz <rchrdgrcordaz@gmail.com>
2024-05-08 16:12:56 -0600

    subir portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 import astroI18next from "astro-i18next";
 
+import { defineConfig } from 'astro/config';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next(),tailwind()],
+  site: 'https://richardgarciaordaz.github.io', // Reemplaza 'astronaut' con tu nombre de usuario de GitHub
+  base: 'richardgarciaordazux.github.io'
+
 });
