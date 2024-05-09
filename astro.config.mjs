e72071033f894349a3295aae351e12238acedc79
richardgarciaordaz <rchrdgrcordaz.com>
2024-05-08 18:36:05 -0600

    Subir todo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import astroI18next from "astro-i18next";
-
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(),tailwind()],
-  site: 'https://richardgarciaordaz.github.io', // Reemplaza 'astronaut' con tu nombre de usuario de GitHub
-  base: 'richardgarciaordazux.github.io'
+  integrations: [tailwind()],
+  site: 'https://richardgarciaordazux.github.io' // Reemplaza 'astronaut' con tu nombre de usuario de GitHub
 
 });
