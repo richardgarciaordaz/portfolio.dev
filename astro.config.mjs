35e02a076247681d41ae0e1efe282e01b70fab3f
Richard Garcia <64115217+richardgarciaordaz@users.noreply.github.com>
2024-09-11 06:41:55 +0000

    cambios nuevos

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import tailwind from "@astrojs/tailwind";
 
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
+  output: 'server',
+  adapter: vercel(),
   site: 'https://richardgarciaordaz.github.io', // Reemplaza 'astronaut' con tu nombre de usuario de GitHub
   base: 'portfolio.dev'
 
